2c34edf36ace7adf77a277409530d293b330cd6f
Максим Комозин <m.i.komozin@gmail.com>
2022-07-19 20:09:16 +0700

    chahged success 19/07/22

--- src/main/java/com/example/MyBookShopApp/controllers/BooksController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/BooksController.java
@@ -1,29 +1,45 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.ResourceStorage;
 import com.example.MyBookShopApp.data.entity.Book;
 import com.example.MyBookShopApp.data.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
 
     private final BookRepository bookRepository;
+    private final ResourceStorage storage;
 
     @Autowired
-    public BooksController(BookRepository bookRepository) {
+    public BooksController(BookRepository bookRepository, ResourceStorage storage) {
         this.bookRepository = bookRepository;
+        this.storage = storage;
     }
+
 
     @GetMapping("/{slug}")
     public String bookPage(@PathVariable("slug") String slug, Model model) {
         Book book = bookRepository.findBooksBySlug(slug);
         model.addAttribute("slugBook", book);
         return "/books/slug";
+    }
+
+    @PostMapping("/{slug}/img/save")
+    public String saveNewBooksImage(@RequestParam("file")MultipartFile file, @PathVariable("slug") String slug) throws IOException {
+
+        String savePath = storage.saveNewBookImage(file, slug);
+        Book bookToUpdate = bookRepository.findBooksBySlug(slug);
+        bookToUpdate.setImage(savePath);
+        bookRepository.save(bookToUpdate); //save new image for book in db
+
+        return "redirect:/books/" + slug;
     }
 }
